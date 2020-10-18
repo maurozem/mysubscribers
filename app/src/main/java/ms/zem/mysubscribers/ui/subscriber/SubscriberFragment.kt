b33a402175286a1245cb231aec3839b1f39082e9
maurozem <maurozem@brq.com>
2020-10-18 15:27:24 -0300

    implementação do insert com o fragment/viewmodel desacoplados

--- app/src/main/java/ms/zem/mysubscribers/ui/subscriber/SubscriberFragment.kt
+++ app/src/main/java/ms/zem/mysubscribers/ui/subscriber/SubscriberFragment.kt
@@ -1,21 +1,75 @@
 package ms.zem.mysubscribers.ui.subscriber
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.subscriber_fragment.*
 import ms.zem.mysubscribers.R
+import ms.zem.mysubscribers.data.db.AppDatabase
+import ms.zem.mysubscribers.data.db.dao.SubscriberDAO
+import ms.zem.mysubscribers.extension.hideKeyboard
+import ms.zem.mysubscribers.repository.DatabaseDataSource
+import ms.zem.mysubscribers.repository.SubscriberRepository
 
-class SubscriberFragment : Fragment() {
+class SubscriberFragment : Fragment(R.layout.subscriber_fragment) {
 
-    private lateinit var viewModel: SubscriberViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.subscriber_fragment, container, false)
+    private val viewModel: SubscriberViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val subscriberDAO: SubscriberDAO =
+                    AppDatabase.getInstance(requireContext()).subscriberDAO
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDAO)
+                return SubscriberViewModel(repository) as T
+            }
+        }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvents()
+        setListners()
+    }
+
+    private fun setListners() {
+        btn_add.setOnClickListener {
+            val name = tiedt_name.text.toString()
+            val email = tiedt_email.text.toString()
+
+            viewModel.addSubscriber(name, email)
+        }
+    }
+
+    private fun observeEvents() {
+        viewModel.subscriberStateEventData.observe(viewLifecycleOwner){ subscribeState ->
+            when (subscribeState) {
+                is SubscriberViewModel.SubscriberState.Inserted -> {
+                    clearFields()
+                    hideKeyboard()
+                }
+            }
+        }
+        viewModel.messageEventData.observe(viewLifecycleOwner){ stringResId ->
+            Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun hideKeyboard() {
+        val parentActivity = requireActivity()
+        if (parentActivity is AppCompatActivity){
+            parentActivity.hideKeyboard()
+        }
+    }
+
+    private fun clearFields() {
+        tiedt_name.text?.clear()
+        tiedt_email.text?.clear()
+    }
+
 
 }
--- app/src/main/java/ms/zem/mysubscribers/ui/subscriber/SubscriberViewModel.kt
+++ app/src/main/java/ms/zem/mysubscribers/ui/subscriber/SubscriberViewModel.kt
@@ -1,7 +1,46 @@
 package ms.zem.mysubscribers.ui.subscriber
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ms.zem.mysubscribers.R
+import ms.zem.mysubscribers.repository.SubscriberRepository
 
-class SubscriberViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SubscriberViewModel(
+    private val repository: SubscriberRepository
+) : ViewModel() {
+
+    private val _subscriberStateEventData = MutableLiveData<SubscriberState>()
+    val subscriberStateEventData: LiveData<SubscriberState>
+        get() = _subscriberStateEventData
+
+    private val _messageEventData = MutableLiveData<Int>()
+    val messageEventData: LiveData<Int>
+        get() = _messageEventData
+
+    fun addSubscriber(name: String, email: String) = viewModelScope.launch {
+        try {
+            val id = repository.insertSubscriber(name, email)
+            if ( id > 0 ){
+                _subscriberStateEventData.value = SubscriberState.Inserted
+                _messageEventData.value = R.string.subscriber_inserted_sucessfully
+            } else {
+                _messageEventData.value = R.string.subscriber_error_to_insert
+            }
+        } catch (ex: Exception){
+            Log.e(TAG, ex.toString())
+            _messageEventData.value = R.string.subscriber_error_to_insert
+        }
+    }
+
+    sealed class SubscriberState{
+        object Inserted : SubscriberState()
+    }
+
+    companion object{
+        private val TAG = SubscriberViewModel::class.java.simpleName
+    }
 }
